6a1ce44ef942ebf78286eecef4dffb96c7e42b54
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-17 19:21:59 -0300

    updating prediction

--- regression/linear-regretion.py
+++ regression/linear-regretion.py
@@ -16,14 +16,6 @@ from matplotlib import style
 
 style.use("fivethirtyeight")
 
-# np array
-xs = np.array([1, 2, 3, 4, 5, 6], dtype = np.float64)
-ys = np.array([5, 4, 6, 5, 6, 7], dtype = np.float64)
-
-
-
-
-
 def best_fit_slop_and_intercept(xs, ys):
     m = ( ((mean(xs) * mean(ys)) - (mean(xs*ys))) /
           ((mean(xs)**2) - mean(xs**2)) )
@@ -32,12 +24,21 @@ def best_fit_slop_and_intercept(xs, ys):
 
     return m, b
 
+if __name__ == "__main__":
+        
+    # np array
+    xs = np.array([1, 2, 3, 4, 5, 6], dtype = np.float64)
+    ys = np.array([5, 4, 6, 5, 6, 7], dtype = np.float64)
+
+    m, b = best_fit_slop_and_intercept(xs, ys)
+    regression_line = [(m*x) + b for x in xs]
+
+    predict_x = 8
+
+    predict_y = (m*predict_x) + b
 
 
-m, b = best_fit_slop_and_intercept(xs, ys)
-
-regression_line = [(m*x) + b for x in xs]
-
-plt.scatter(xs, ys)
-plt.plot(xs, regression_line)
-plt.show()
+    plt.scatter(xs, ys)
+    plt.scatter(predict_x, predict_y)
+    plt.plot(xs, regression_line)
+    plt.show()
