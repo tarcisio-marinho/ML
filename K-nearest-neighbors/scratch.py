70f8ebd81deed057b614a2c0ee73c653493b49cc
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-18 00:32:24 -0300

    updating cancer test scratch knn implementation

--- K-nearest-neighbors/scratch.py
+++ K-nearest-neighbors/scratch.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 from collections import Counter
 import warnings
+import pandas as pd
+import random
 
 style.use("fivethirtyeight")
 
@@ -58,10 +60,38 @@ def test():
     plt.show()
 
 def cancer_test():
-    pass
+    df = pd.read_csv("data/breast-cancer-wisconsin.data")
+    df.replace("?", -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+    random.shuffle(full_data)
 
+    test_size = 0.2
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
+    train_data = full_data[:-int(test_size * len(full_data))]
+    test_data = full_data[-int(test_size * len(full_data)):]
+    
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
+
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
+
+    correct = 0
+    total = 0
+    for group in test_set:
+        for data in test_set[group]:
+            vote = knn(train_set, data, k=5)
+            if(group == vote):
+                correct+=1
+            total+=1
+
+    print("Accuracy: " , correct/total)
+        
 
 if __name__ == "__main__":
-    test()
+    #test()
+    cancer_test()
 
     
