ab1865bc53b1d8c0bdeb6c72c27e4f508229fdbc
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-17 23:20:52 -0300

    knn from scratch with mine own dataset

--- K-nearest-neighbors/scratch.py
+++ K-nearest-neighbors/scratch.py
@@ -10,9 +10,14 @@ sqrt((1 - 2)**2 + (3 - 5)**2)
 '''
 
 from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+from collections import Counter
+import warnings
 
-q = [1, 3]
-p = [2, 5]
+style.use("fivethirtyeight")
+
 
 def euclidian_distance(plot1, plot2):
     sum = 0.0
@@ -23,4 +28,37 @@ def euclidian_distance(plot1, plot2):
 
     return sqrt(sum)
 
-print(euclidian_distance(q, p))
+def knn(data, predict, k=3):
+    if(len(data) >= k):
+        warnings.warn("k is set to a value less than total voting groups.")
+    
+    distances = []
+
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
+            distances.append([euclidean_distance, group])
+
+    votes = [i[1] for i in sorted(distances) [:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+
+    return vote_result
+
+def test():
+    dataset = {'k' : [[1, 2], [2, 3], [3, 1]], 'r':[[6, 5], [7, 7], [8, 6]]}
+    new_features = [5, 7]
+    result = knn(dataset, new_features, k=3)
+    print("the new feature is near to group: " + result)
+
+    for i in dataset:
+        for ii in dataset[i]:
+            plt.scatter(ii[0], ii[1], s=100, color=i)
+    
+    plt.scatter(new_features[0], new_features[1])
+    plt.show()
+
+
+if __name__ == "__main__":
+    test()
+
+    
