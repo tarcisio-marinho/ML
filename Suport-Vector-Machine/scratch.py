f167c1c0ff22d9917190f1bf018fcbbc0ad852a1
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-18 23:54:38 -0300

    adding suport vector machine

---
+++ Suport-Vector-Machine/scratch.py
@@ -0,0 +1,21 @@
+# U*W + b > 0 || < 0 || == 0
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+        
+df = pd.read_csv('data/breast-cancer-wisconsin.data')
+
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True) # remove ID
+
+x = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+for a in x:
+    plt.scatter(a)
+
+for b in y:
+    plt.scatter(b)
+
+plt.show()
---
+++ Suport-Vector-Machine/svm.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors, svm
+import pandas as pd
+
+
+
+
+if __name__ == "__main__":
+        
+    df = pd.read_csv('data/breast-cancer-wisconsin.data')
+
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True) # remove ID
+
+    x = np.array(df.drop(['class'], 1))
+    y = np.array(df['class'])
+
+    x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size=0.2)
+    clf = svm.SVC()
+    clf.fit(x_train, y_train)
+
+    accuracy = clf.score(x_test, y_test)
+    print(accuracy)
+
+    # test
+    example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 2, 2, 2, 3, 2, 1]])
+    example_measures = example_measures.reshape(len(example_measures), -1)
+
+    prediction = clf.predict(example_measures)
+    print(prediction) 
+    # 98% de certeza se o cancer vai ser maligno ou benigno
