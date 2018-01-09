9e1187813d758964531a63551b4e5f1004158a9c
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-01-09 19:14:01 -0300

    kmeans from scratch

--- K-Means/scratch.py
+++ K-Means/scratch.py
@@ -1,15 +1,30 @@
 import numpy as np
 import pandas as pd
-import matplotlib.ploty as plt
+import matplotlib.pyplot as plt
 from matplotlib import style
 
-style.use("ggplot")
+class KMeans:
+    def __init__(self, k=2, tol=0.001, max_itr=300):
+        self.k = k
+        self.tol = tol
+        self.max_itr = max_iter
+    
+    def fit(self, data):
+        pass
 
-x = np.array([[1, 2],
-              [1.5, 1.8],
-              [8, 8],
-              [1, 0.6],
-              [9, 11]])
+    def predict(self, data):
+        pass
 
-plt.scatter(x[:,0], x[:,-1], s=150)
-plt.show()
+
+if __name__ == "__main__":
+    style.use("ggplot")
+
+    x = np.array([[1, 2],
+                [1.5, 1.8],
+                [8, 8],
+                [1, 0.6],
+                [9, 11]])
+
+    plt.scatter(x[:,0], x[:,-1], s=150)
+    plt.show()
+
